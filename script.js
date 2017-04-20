a128b671f67f5d2ef554b3aedfe4ee5db6399c55
a2pr <a2payema@gmail.com>
2017-04-20 16:30:48 -0400

    ekem.MK1.2

--- script.js
+++ script.js
@@ -4,11 +4,20 @@
   
   var FirstController = function ($scope) {
       
-     /* var OnStart= function(){
-        var start= true ;
-      };*/
+     var OnStart= function(){
+        $scope.start= true ;
+        $scope.message1= randomNumber(eventList.length -1)
+      };
+      function randomNumber(range){
+            return Math.round( Math.random() * range );
+        }
+        var  eventList=[
+            "You woke up in a cementary, with nothing in your pockets",
+            "You open your eyes and feel the freeze cold wind of the glaciar",
+            "You appear in the middle of a forest, with trees as high as the eye can see"
+        ];
       $scope.message ="Hi, time to start your new adventure!"; 
-      $scope.message1= "history";
+     
 };
 
 app.controller("FirstController", FirstController);
